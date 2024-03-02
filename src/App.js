3110b679185663147789248359445754784bd521
Solamatin Mikhail <solamatinm@smart-ui.pro>
2024-03-02 19:24:59 +0100

    starter commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './kh-ski.png';
 import './App.css';
 
 function App() {
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br />
+        <br />
+        <h1>
+          Before and After
+        </h1>
       </header>
     </div>
   );
